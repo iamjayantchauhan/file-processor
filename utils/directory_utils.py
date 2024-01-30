d9ddc5025e8e5623fb345da7c113f30e3c6dc8e6
Rutvi3 <rutvipadariya2933@gmail.com>
2024-01-30 17:31:21 +0530

    refactor(utils): add directory related utility function
    function to copy files

---
+++ utils/directory_utils.py
@@ -0,0 +1,18 @@
+"""
+    This module provides a set of utility functions for common tasks related to directory operations.
+"""
+
+import os
+import shutil
+from pathlib import Path
+
+
+def copy_files(source_directory: str, target_directory: str, files: list):
+    """
+    Copy given files from one directory to another directory
+    :param source_directory: path of directory from where files to be copied
+    :param target_directory: path of directory where copied files to be pasted
+    :param files: list of files to be copied
+    """
+    for file in files:
+        shutil.copy(os.path.join(source_directory, file), os.path.join(target_directory, file))
