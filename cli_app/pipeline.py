957d3b0626b84b01508965ce0fc1fd170c2c7000
Jinesh Patel <jineshpatel2002@gmail.com>
2024-01-30 15:08:17 +0530

    refactor: initialize pipline with changes

--- cli_app/pipeline.py
+++ cli_app/pipeline.py
@@ -2,15 +2,14 @@ import os
 
 
 class Processor:
-    def __init__(self):
-        # Input method
-
-
-
-
+    def __init__(self, path: str, file_type: str, filename_format: str):
         # Variable declaration
-        self.source_dir = None
-        self.target_dir = None
+        self.path = path
+        self.file_type = file_type
+        self.filename_format = filename_format
+        self.source_dir = os.path.join(self.path, 'source')
+        self.target_dir = os.path.join(self.path, 'target')
+
 
         # Pipline function call
 
