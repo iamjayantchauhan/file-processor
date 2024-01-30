64f13d54460e1381c38e0cffbdf401f2ff540594
Jinesh Patel <jineshpatel2002@gmail.com>
2024-01-30 14:27:02 +0530

    refactor: initialize pipline

---
+++ cli_app/pipeline.py
@@ -0,0 +1,19 @@
+import os
+
+
+class Processor:
+    def __init__(self):
+        # Input method
+
+
+
+
+        # Variable declaration
+        self.source_dir = None
+        self.target_dir = None
+
+        # Pipline function call
+
+
+
+#  Validate directory Methods
