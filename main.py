e47c04799bdc9c4945d5b0a6b8a677e5b431608a
Kunj Gandhi <106432344+kunj-gandhi889@users.noreply.github.com>
2024-02-01 10:55:00 +0530

    Feature/one: added user input and setup entry point in main.py (#8)
    * refactor(hooks): update pre-commit hooks list
    
    * refactor: add dependency group for pre-commit & pylint
    
    * feat: add choices for valid filetypes & filenames
    
    * feat: interactive input using click & update main.py
    
    * refactor: return complete filename format instead of abbreviation

--- main.py
+++ main.py
@@ -2,5 +2,23 @@
 Main file for the project. Only take input and process pipeline here.
 """
 
+import sys
+import click
+from cli_app.pipeline import Processor
+from utils import inputs
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+
+    try:
+        path, file_format, name_format = inputs.get_input(standalone_mode=False)
+        pipeline = Processor(path, file_format, name_format)
+        pipeline.process_directory()
+    except click.exceptions.Abort:
+        print(f"\nForced Stopped \t\t\t python {sys.argv[0]}")
+    except click.exceptions.NoSuchOption as e:
+        print(f"\nInvalid Option: {e}")
+    except click.exceptions.BadParameter as e:
+        print(f"\nInvalid Parameter: {e}")
+    except click.exceptions.UsageError as e:
+        print(f"\nInvalid Usage: {e}")
